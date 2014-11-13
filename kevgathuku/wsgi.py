3c280f99590e6bb42a1731d3f7ba9324a979e735
Kevin Ndung'u <kevgathuku@gmail.com>
2014-11-13 22:41:45 +0300

    Add whitenoise settings for static files serving

--- kevgathuku/wsgi.py
+++ kevgathuku/wsgi.py
@@ -11,6 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kevgathuku.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
